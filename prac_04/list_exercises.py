c724b8821332461cff325b562724a009d482c2f3
KierenJackson <kieren.jacksonclapper@my.jcu.edu.au>
2019-08-22 18:16:11 +1000

    Add username checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,22 @@
-numbers = []
+# numbers = []
+#
+# for number in range(1, 6):
+#     numbers.append(int(input("Please enter a number: ")))
+#
+# sum_of_numbers = sum(numbers)
+# average = sum_of_numbers/5
+#
+# print("The first number is {}".format(numbers[0]))
+# print("The last number is {}".format(numbers[-1]))
+# print("The lowest number is {}".format(min(numbers)))
+# print("The highest number is {}".format(max(numbers)))
+# print("The sum of the numbers is {}".format(sum_of_numbers))
+# print("The average of the numbers is {}".format(average))
 
-for number in range(1, 6):
-    numbers.append(int(input("Please enter a number: ")))
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-sum_of_numbers = sum(numbers)
-average = sum_of_numbers/5
-
-print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[-1]))
-print("The lowest number is {}".format(min(numbers)))
-print("The highest number is {}".format(max(numbers)))
-print("The sum of the numbers is {}".format(sum_of_numbers))
-print("The average of the numbers is {}".format(average))
-
+username = input("Please enter your username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
